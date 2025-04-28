6cbedb15311deb86716bacefae9b45b37f6a7884
BobessA <adria72n@gmail.com>
2025-04-28 21:56:23 +0200

    Jármű végpontok,

--- Backend/CarService/CarService/Controllers/VehiclesController.cs
+++ Backend/CarService/CarService/Controllers/VehiclesController.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using CarService.Models;
+using CarService.DTOs;
+using CarService.Helpers;
+using Azure.Core;
 
 namespace CarService.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/Vehicles")]
     [ApiController]
+    [Consumes("application/json")]
+    [Produces("application/json")]
     public class VehiclesController : ControllerBase
     {
         private readonly CarServiceContext _context;
@@ -15,10 +20,181 @@ namespace CarService.Controllers
             _context = context;
         }
 
+        /// <summary>
+        /// Járművek lekérése
+        /// </summary>
+        /// <param name="vehicleId">vehicle.id</param>
+        /// <param name="userId">user.id</param>
+        /// <param name="cToken">CancellationToken</param>
+        /// <returns>VehiclesDTO list, 400, 204</returns>
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Vehicle>>> GetVehicles()
+        [ProducesResponseType(typeof(List<VehiclesDTO>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> GetVehicles(int? vehicleId, string? userId, CancellationToken cToken)
         {
-            return await _context.Vehicles.ToListAsync();
+            var query = _context.Vehicles.AsQueryable();
+
+            if (vehicleId.HasValue)
+            {
+                query = query.Where(v => v.Id == vehicleId.Value);
+            }
+
+            if (!string.IsNullOrEmpty(userId))
+            {
+                if (Guid.TryParse(userId, out Guid parsedUserId))
+                {
+                    query = query.Where(v => v.OwnerId == parsedUserId);
+                }
+                else
+                {
+                    return BadRequest("Invalid userId format.");
+                }
+            }
+
+            List<VehiclesDTO> vehicles = await query
+                .Select(v => new VehiclesDTO
+                {
+                    id = v.Id,
+                    ownerId = v.OwnerId,
+                    licensePlate = v.LicensePlate,
+                    brand = v.Brand,
+                    model = v.Model,
+                    yearOfManufacture = v.YearOfManufacture,
+                    vin = v.Vin,
+                    engineCode = v.EngineCode,
+                    odometer = v.Odometer,
+                    fuelType = v.FuelTypeId,
+                })
+                .ToListAsync(cToken);
+
+            if (vehicles == null || vehicles.Count == 0)
+            {
+                return NoContent();
+            }
+
+            return Ok(vehicles);
+        }
+
+        /// <summary>
+        /// Jármű rögzítés
+        /// </summary>
+        /// <param name="request">PostVehicleRequest</param>
+        /// <param name="cToken">CancellationToken</param>
+        /// <returns>200, 400</returns>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> PostVehicle([FromBody] PostVehicleRequest request, CancellationToken cToken)
+        {
+            if (!await _context.Users.AnyAsync(u => u.Id == request.ownerId, cToken))
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, "Owner does not exists");
+            }
+
+            if (!await _context.FuelTypes.AnyAsync(f => f.Id == request.fuelType, cToken))
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, "Invalid Fuel type");
+            }
+
+            var vehicle = new Vehicle
+            {
+                OwnerId = request.ownerId,
+                LicensePlate = request.licensePlate,
+                Brand = request.brand,
+                Model = request.model,
+                YearOfManufacture = request.yearOfManufacture,
+                Vin = request.vin,
+                EngineCode = request.engineCode,
+                Odometer = request.odometer,
+                FuelTypeId = request.fuelType,
+            };
+
+            await _context.Vehicles.AddAsync(vehicle);
+            await _context.SaveChangesAsync(cToken);
+
+            return Ok();
+        }
+
+        /// <summary>
+        /// Jármű adatok módosítása
+        /// </summary>
+        /// <param name="request">UpdateVehicleRequest</param>
+        /// <param name="cToken">CancellationToken</param>
+        /// <returns>200, 400, 204</returns>
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> PutVehicle([FromBody] UpdateVehicleRequest request, CancellationToken cToken)
+        {
+            var vehicle = await _context.Vehicles.FirstOrDefaultAsync(u => u.Id == request.id, cToken);
+
+            if (vehicle == null)
+            {
+                return NoContent();
+            }
+
+            if (request.ownerId != null && !await _context.Users.AnyAsync(u => u.Id == request.ownerId, cToken))
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, "Owner does not exists");
+            }
+
+            vehicle.OwnerId = request.ownerId ?? vehicle.OwnerId;
+            vehicle.Odometer = request.odometer ?? vehicle.Odometer;
+
+            try
+            {
+                await _context.SaveChangesAsync(cToken);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// Jármű törlés
+        /// </summary>
+        /// <param name="id">vehicles.id</param>
+        /// <param name="cToken">CancellationToken</param>
+        /// <returns>204, 400, 204</returns>
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> DeleteVehicle(int id, CancellationToken cToken)
+        {
+            var vehicle = await _context.Vehicles.FirstOrDefaultAsync(v => v.Id == id, cToken);
+
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            if (await _context.OrdersHeaders.AnyAsync(o => o.VehicleId == id, cToken))
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, "Vehicle is already listed on an order");
+            }
+
+            if (await _context.Offers.AnyAsync(o => o.VehicleId == id, cToken))
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, "Vehicle is already listed on an offer");
+            }
+
+            try
+            {
+                _context.Vehicles.Remove(vehicle);
+                await _context.SaveChangesAsync();
+
+                return NoContent();
+            } 
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, ex.Message);
+            }
         }
     }
 }
---
+++ Backend/CarService/CarService/DTOs/VehiclesDTO.cs
@@ -0,0 +1,208 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarService.DTOs
+{
+    /// <summary>
+    /// Járművek
+    /// </summary>
+    [Serializable]
+    public class VehiclesDTO
+    {
+        /// <summary>
+        /// Jármű azonosító
+        /// </summary>
+        /// <example>1</example>
+        [Display(Name = "Jármű azonosító")]
+        [Required]
+        public int id { get; set; }
+
+        /// <summary>
+        /// Ügyfél azonosító
+        /// </summary>
+        /// <example>561104E8-52E0-4210-B949-CAA54FCCF873</example>
+        [Display(Name = "Ügyfél azonosító")]
+        [Required]
+        public Guid ownerId { get; set; }
+
+        /// <summary>
+        /// Rendszám
+        /// </summary>
+        /// <example>XYZ-001</example>
+        [Display(Name = "Rendszám")]
+        [StringLength(10)]
+        [Required]
+        public string licensePlate { get; set; }
+
+        /// <summary>
+        /// Gyártó
+        /// </summary>
+        /// <example>Tesla</example>
+        [Display(Name = "Gyártó")]
+        [StringLength(32)]
+        [Required]
+        public string brand {  get; set; }
+
+        /// <summary>
+        /// Modell
+        /// </summary>
+        /// <example>Model 3</example>
+        [Display(Name = "Modell")]
+        [StringLength(32)]
+        [Required]
+        public string model { get; set; }
+
+        /// <summary>
+        /// Évjárat
+        /// </summary>
+        /// <example>2020</example>
+        [Display(Name = "Évjárat")]
+        [Required]
+        public short yearOfManufacture { get; set; }
+
+        /// <summary>
+        /// Alvázszám
+        /// </summary>
+        /// <example>xxx73452jhaopskve</example>
+        [Display(Name = "Alvázszám")]
+        [StringLength(17)]
+        [Required]
+        public string vin {  get; set; }
+
+        /// <summary>
+        /// Motorkód
+        /// </summary>
+        /// <example>ergbas</example>
+        [Display(Name = "Motorkód")]
+        [StringLength(20)]
+        public string? engineCode { get; set; }
+
+        /// <summary>
+        /// Kilóméteróra állás
+        /// </summary>
+        /// <example>23000</example>
+        [Display(Name = "Kilóméteróra állás")]
+        public int? odometer { get; set; }
+
+        /// <summary>
+        /// Üzemanyag típus
+        /// </summary>
+        /// <example>3</example>
+        [Display(Name = "Üzemanyag típus")]
+        [Required]
+        public int fuelType { get; set; }
+    }
+
+    /// <summary>
+    /// Jármű rögzítés
+    /// </summary>
+    [Serializable]
+    public class PostVehicleRequest
+    {
+        /// <summary>
+        /// Ügyfél azonosító
+        /// </summary>
+        /// <example>561104E8-52E0-4210-B949-CAA54FCCF873</example>
+        [Display(Name = "Ügyfél azonosító")]
+        [Required]
+        public Guid ownerId { get; set; }
+
+        /// <summary>
+        /// Rendszám
+        /// </summary>
+        /// <example>XYZ-001</example>
+        [Display(Name = "Rendszám")]
+        [StringLength(10)]
+        [Required]
+        public string licensePlate { get; set; }
+
+        /// <summary>
+        /// Gyártó
+        /// </summary>
+        /// <example>Tesla</example>
+        [Display(Name = "Gyártó")]
+        [StringLength(32)]
+        [Required]
+        public string brand { get; set; }
+
+        /// <summary>
+        /// Modell
+        /// </summary>
+        /// <example>Model 3</example>
+        [Display(Name = "Modell")]
+        [StringLength(32)]
+        [Required]
+        public string model { get; set; }
+
+        /// <summary>
+        /// Évjárat
+        /// </summary>
+        /// <example>2020</example>
+        [Display(Name = "Évjárat")]
+        [Required]
+        public short yearOfManufacture { get; set; }
+
+        /// <summary>
+        /// Alvázszám
+        /// </summary>
+        /// <example>xxx73452jhaopskve</example>
+        [Display(Name = "Alvázszám")]
+        [StringLength(17)]
+        [Required]
+        public string vin { get; set; }
+
+        /// <summary>
+        /// Motorkód
+        /// </summary>
+        /// <example>ergbas</example>
+        [Display(Name = "Motorkód")]
+        [StringLength(20)]
+        public string? engineCode { get; set; }
+
+        /// <summary>
+        /// Kilóméteróra állás
+        /// </summary>
+        /// <example>23000</example>
+        [Display(Name = "Kilóméteróra állás")]
+        public int? odometer { get; set; }
+
+        /// <summary>
+        /// Üzemanyag típus
+        /// </summary>
+        /// <example>3</example>
+        [Display(Name = "Üzemanyag típus")]
+        [Required]
+        public int fuelType { get; set; }
+    }
+
+    /// <summary>
+    /// Jármű adatok módosítása
+    /// </summary>
+    [Serializable]
+    public class UpdateVehicleRequest
+    {
+        /// <summary>
+        /// Jármű azonosító
+        /// </summary>
+        /// <example>1</example>
+        [Display(Name = "Jármű azonosító")]
+        [Required]
+        public int id { get; set; }
+
+        /// <summary>
+        /// Ügyfél azonosító
+        /// </summary>
+        /// <example>561104E8-52E0-4210-B949-CAA54FCCF873</example>
+        [Display(Name = "Ügyfél azonosító")]
+        public Guid? ownerId { get; set; }
+
+        /// <summary>
+        /// Kilóméteróra állás
+        /// </summary>
+        /// <example>23000</example>
+        [Display(Name = "Kilóméteróra állás")]
+        public int? odometer { get; set; }
+
+    }
+
+}
